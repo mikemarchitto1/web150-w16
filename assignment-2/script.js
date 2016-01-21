defbc253c3af7e83782b522bb94c8568933317b9
mikemarchitto1 <mikemarchitto@gmail.com>
2016-01-20 17:26:48 -0800

    Assignment 2
    Notes

--- assignment-2/script.js
+++ assignment-2/script.js
@@ -1,13 +1,28 @@
-// JavaScript Document
+var enter = confirm("You're at the gates of hell. Press OK to open and go in.");
 
-function set_matching_word(selectObj, txtObj)
-{
-   var number = txtObj.value;
-   for(var i = 0; i < selectObj.length; i++)
-   {
-      if(selectObj.options[i].value == number)
-      selectObj.selectedIndex = i;
-   }
+if (enter) {
+	//go into garage
+	var gates = prompt("You see 3 demons named Zufolous, Jecarza and Becrolock. Pick one.")
+	if (gates == 'Zufolous') {
+    	console.log("Your dead! THE END");
+	} else if (gates == 'Jecarza') {
+    	console.log("You wake up! THE END");
+	} else {
+    	console.log("Into heaven you go!");
+   	 
+    	var centuries = "You have to go to purgatory. How many centuries are you going to be there?";
+    	var away = prompt(centuries);
+    	away = Number(away);
+   	 
+    	if (away <= 2) {
+        	console.log("You wake up and your alive!");
+    	} else {
+        	console.log("You burn in hell for eternity.");
+    	}
+    	console.log("THE END.");
+   	 
+	}
+} else {
+	//didn't go in
+	console.log("And that's life. THE END.")
 }
-
-
